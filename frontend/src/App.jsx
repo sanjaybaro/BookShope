3154436b49a7e2b1cc6d135d66658a4dbf6f4732
Sanjay Baro <sanjaybaro2017@gmail.com>
2024-06-12 19:30:04 +0530

    frontend first phase setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Banner from "./Components/Banner";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Banner/>
+      <Footer/>
+    </>
+  );
+};
+
+export default App;
