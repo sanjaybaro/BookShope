f89c35cd6e309425da46e43423f2fc93d647cac4
Sanjay Baro <sanjaybaro2017@gmail.com>
2024-06-15 20:52:14 +0530

    Frontend  Done

--- frontend/src/Components/Cards.jsx
+++ frontend/src/Components/Cards.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Cards({ item }) {
   return (
     <>
-      <div className="mt-4 my-3 p-3">
-        <div className="card w-92 bg-base-100 shadow-xl hover:scale-105 duration-200  dark:bg-slate-100 dark:text-black ">
+      <div className="mt-4 my-3 p-3 ">
+        <div className="card w-92 bg-base-100 shadow-xl border hover:scale-105 duration-200  dark:bg-slate-100 dark:text-black">
           <figure>
             <img src={item.image} alt="Shoes" />
           </figure>
