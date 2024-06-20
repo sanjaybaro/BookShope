26551f4a593909f298ea876c051a2ce3b4334d1f
Sanjay Baro <sanjaybaro2017@gmail.com>
2024-06-20 13:58:23 +0530

    routes added for login and signup

--- backend/Controller/user.contoller.js
+++ backend/Controller/user.contoller.js
@@ -16,7 +16,14 @@ export const signup = async (req, res) => {
       password: hashPassword,
     });
     await createdUser.save();
-    res.status(201).json({ message: "User created successfully" });
+    res.status(201).json({
+      message: "User created successfully",
+      user: {
+        _id: createdUser._id,
+        fullname: createdUser.fullname,
+        email: createdUser.email,
+      },
+    });
   } catch (error) {
     console.log("Error: " + error.message);
     res.status(500).json({ message: "Internal server error" });
@@ -28,7 +35,7 @@ export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-    const isMatch = bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(password, user.password);
     if (!user || !isMatch) {
       return res.status(400).json({ message: "Invalid username or password" });
     } else {
